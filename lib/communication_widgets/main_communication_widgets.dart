78e0fb71bec7584b152fd0d5d44f0466be161a6d
ShamiLuna <140070726+shamiluna@users.noreply.github.com>
2024-06-20 12:31:43 +0530

    neccessary to animate allanimated name of project

---
+++ lib/communication_widgets/main_communication_widgets.dart
@@ -0,0 +1,18 @@
+import 'package:animatedlistpage/communication_widgets/parent_page.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter_samples/communication_widgets/parent_page.dart';
+
+class MainCommunicationWidgets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData.dark(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Communication Widgets"),
+        ),
+        body: ParentPage(),
+      ),
+    );
+  }
+}
---
+++ lib/persistent_tabbar/main_persistent_tabbar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../hero_animations/page1.dart';
+import '../hero_animations/page2.dart';
+// import 'package:flutter_samples/persistent_tabbar/page1.dart';
+// import 'package:flutter_samples/persistent_tabbar/page2.dart';
+
+class MainPersistentTabBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.directions_car),
+                text: "Non persistent",
+              ),
+              Tab(icon: Icon(Icons.directions_transit), text: "Persistent"),
+              Tab(icon: Icon(Icons.directions_railway), text: "Test 1"),
+              Tab(icon: Icon(Icons.directions_subway), text: "Test 2"),
+            ],
+          ),
+          title: Text('Persistent Tab Demo'),
+        ),
+        body: TabBarView(
+          children: [
+            Page1(),
+            Page2(),
+            Text('Page 3'),
+            Text('Page 4'),
+          ],
+        ),
+      ),
+    );
+  }
+}
